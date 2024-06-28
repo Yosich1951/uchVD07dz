a4782bfc71681c9c9819d4a482281907587ee639
Viktor <viktor-iskalin@yandex.ru>
2024-06-28 13:49:03 +0300

    add version 1

---
+++ app/routes.py
@@ -0,0 +1,66 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from app import db, bcrypt
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from app.models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
+main = Blueprint('main', __name__)
+
+@main.route("/")
+@main.route("/home")
+def home():
+    return render_template('home.html')
+
+@main.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('main.login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@main.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash('Войти не удалось. Пожалуйста, проверьте адрес электронной почты и пароль', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@main.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+@main.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.password.data:
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            current_user.password = hashed_password
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Ваш аккаунт изменен успешно', 'success')
+        return redirect(url_for('main.account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('edit_profile.html', title='Account', form=form)
+
